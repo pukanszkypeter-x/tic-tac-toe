5e32335204dce274cf61d998b4dfe20a262708ee
Pukánszky Péter <peter.pukanszky@accenture.com>
2022-10-06 17:00:45 +0200

    React router and login page added.

---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export class Login extends React.Component {
+    render() {
+        return (
+            <div className='login-container'>
+                <div className='place-holder-1'></div>
+                <img id='login-logo' alt='logo' height='100px' width='100px' src='logo192.png'/>
+                <div className='place-holder-1'></div>
+            </div>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom/client';
+import Particles from 'particlesjs';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate
+  } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import {Game} from './components/Game';
+import {Login} from './components/Login';
+import {Error} from './components/Error';
+
+class App extends Component {
+    constructor() {
+      super();
+      this.state = {
+        isUserAuthenticated: false
+      };
+    }
+  
+    render() {
+        window.onload = function() {
+            Particles.init({
+                selector: '.background',
+                color: 'white'
+            });
+        };
+
+        return (
+        <div>
+          <Router>
+              <Routes>
+                  <Route exact path="/" element={ this.state.isUserAuthenticated ? <Navigate to="/home" /> : <Navigate to="/login" /> }/>
+                  <Route exact path="/home" element={ this.state.isUserAuthenticated ? <Game/> : <Navigate to="/login" /> } />
+                  <Route exact path="/login" element={ this.state.isUserAuthenticated ? <Navigate to="/home" /> : <Login /> } />
+                  <Route exact path="/*" element={ <Error/> } />
+              </Routes>
+          </Router>
+          <canvas className="background"></canvas>
+          <script src="path/to/particles.min.js"></script>
+        </div>
+      );
+    }
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Game />);
+root.render(<App />);
